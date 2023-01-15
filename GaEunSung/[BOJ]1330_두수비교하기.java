63db8d55945cc67ce3190bdb3c04dcc61949fd3c
user <user@LAPTOP-IN1OIPHK>
2023-01-15 21:17:05 +0900

    [BOJ]1330_두수비교하기

---
+++ GaEunSung/[BOJ]1330_두수비교하기.java
@@ -0,0 +1,37 @@
+package my.practice;
+
+import java.util.Scanner;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("숫자를 입력하세요");
+		
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+		
+		sc.close();
+		
+		String result = "";
+		
+		/*
+		if(A>B)
+			result = ">";
+		else if(A<B)
+			result = "<";
+		else
+			result = "=";
+		*/
+		// 삼항연산자의 중첩을 이용
+		result = (A>B?">":(A<B?"<":"="));
+		
+		System.out.println(result);
+		
+		
+
+	}
+
+}
