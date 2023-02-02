8c5041ad396c57d42183c321a5a5fd031489c167
june <ajsjdlwj0123@gmail.com>
2023-02-03 01:50:51 +0900

    [BOJ] DFS와 BFS 1260

--- ParkJuneYub/[BOJ]DFS와_BFS_1260.java
+++ ParkJuneYub/[BOJ]DFS와_BFS_1260.java
@@ -22,7 +22,7 @@ public class Main {
 	public static void main(String args[]) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));		
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		
+		y7yyyyyy
 		v = Integer.parseInt(st.nextToken());
 		e = Integer.parseInt(st.nextToken());
 		int startNode = Integer.parseInt(st.nextToken());
